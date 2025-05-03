8bd55e181a5dcfb244c4cd84791ec4e120f7e6e8
Christopher Johnstone <chrisj97@proton.me>
2025-05-04 00:04:08 +0100

    Added day 01 first solution

---
+++ day_01/distance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser, Namespace
+
+
+def read_input(filepath: str) -> tuple[list[int]]:
+    left: list[int] = []
+    right: list[int] = []
+    with open(filepath, "r") as file:
+        for line in file:
+            left.append(int(line[: line.find(" ")]))
+            right.append(int(line[- line[::-1].find(" "): -1]))
+    left.sort()
+    right.sort()
+    return left, right
+
+
+def main(input_path: str) -> int:
+    left, right = read_input(input_path)
+    distance: int = 0
+    for n in range(len(left)):
+        distance += abs(left[n] - right[n])
+    return distance
+
+
+
+if __name__ == "__main__":
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(dest="path", nargs=1)
+    args: Namespace = parser.parse_args()
+    distance: int = main(args.path[0])
+    print(f"The total distance is {distance}")
