f7501919cbd39970ff241dc24d3c95205bc25376
Christopher Johnstone <chrisj97@proton.me>
2025-05-04 00:08:00 +0100

    Added day 01 part 2

---
+++ day_01/similarity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser, Namespace
+
+
+def read_input(filepath: str) -> tuple[list[int]]:
+    left: list[int] = []
+    right: list[int] = []
+    with open(filepath, "r") as file:
+        for line in file:
+            left.append(int(line[: line.find(" ")]))
+            right.append(int(line[- line[::-1].find(" "): -1]))
+    return left, right
+
+
+def main(input_path: str) -> int:
+    left, right = read_input(input_path)
+    similarity: int = 0
+    for location in left:
+        similarity += location * right.count(location)
+    return similarity
+
+
+
+if __name__ == "__main__":
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(dest="path", nargs=1)
+    args: Namespace = parser.parse_args()
+    similarity: int = main(args.path[0])
+    print(f"The similarity score is {similarity}")
