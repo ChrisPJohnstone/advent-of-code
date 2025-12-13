1b130727b0fa9809dc694631dcca91619f5791a8
Christopher Johnstone <chrisj97@proton.me>
2025-12-13 16:12:27 +0000

    Fixed formatting

--- 2024/16/part_1_recursive.py
+++ 2024/16/part_1_recursive.py
@@ -11,10 +11,10 @@ END_CHAR: str = "E"
 WALL_CHAR: str = "#"
 START_DIRECTION: int = 1
 DIRECTIONS: list[Direction] = [
-    lambda x, y: (x, y - 1),    # N
-    lambda x, y: (x + 1, y),    # E
-    lambda x, y: (x, y + 1),    # S
-    lambda x, y: (x - 1, y),    # W
+    lambda x, y: (x, y - 1),  # N
+    lambda x, y: (x + 1, y),  # E
+    lambda x, y: (x, y + 1),  # S
+    lambda x, y: (x - 1, y),  # W
 ]
 
 
@@ -27,7 +27,7 @@ def char_position(position: int, width: int) -> XY:
     x: int = position % width
     y: int = position // width
     return x, y
-    
+
 
 def maze_spaces(maze: str) -> set[XY]:
     output: set[XY] = set()
@@ -35,7 +35,7 @@ def maze_spaces(maze: str) -> set[XY]:
         for x, char in enumerate(line):
             if char == WALL_CHAR:
                 continue
-            output.add((x,y))
+            output.add((x, y))
     return output
 
 
@@ -46,7 +46,7 @@ def route_scores(
     spaces_visited: set[XY] = set(),
     direction: int = START_DIRECTION,
     steps: int = 0,
-    turns: int = 0
+    turns: int = 0,
 ) -> Iterator[int]:
     if position == end:
         score: int = steps + (turns * 1000)
@@ -75,12 +75,11 @@ def route_scores(
         )
 
 
-
 def main(input_path: str) -> int:
     maze: str = read_input(input_path)
     width: int = maze.find("\n")
-    start: XY = char_position(maze.replace("\n", '').find(START_CHAR), width)
-    end: XY = char_position(maze.replace("\n", '').find(END_CHAR), width)
+    start: XY = char_position(maze.replace("\n", "").find(START_CHAR), width)
+    end: XY = char_position(maze.replace("\n", "").find(END_CHAR), width)
     logging.debug(f"Start {start}. End {end}")
     spaces: set[XY] = maze_spaces(maze)
     return min(list(route_scores(start, end, spaces)))
